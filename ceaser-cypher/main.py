fb6b90bd47312239278a985e859ef8cf18dfe6c6
rock <ratannak.ponn@outlook.com>
2023-04-25 15:14:14 -0500

    Cipher

--- ceaser-cypher/main.py
+++ ceaser-cypher/main.py
@@ -1,10 +1,11 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
             'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = ""
+while direction != "encode" and direction != "decode":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 message = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
 
 def encrypt(message, shift):
     cipher = ""
