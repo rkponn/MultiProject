1af4320b2299ea63411dea260a6f51ec3b861a89
rock <ratannak.ponn@outlook.com>
2023-04-25 15:09:23 -0500

    cipher

---
+++ ceaser-cypher/main.py
@@ -0,0 +1,34 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+message = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(message, shift):
+    cipher = ""
+    for letter in message:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        if new_position > 25:
+            new_position -= 26
+        cipher += alphabet[new_position]
+    print(f"The encoded text is {cipher}")
+
+def decrypt(message, shift):
+    cipher = ""
+    for letter in message:
+        position = alphabet.index(letter)
+        new_position = position - shift
+        if new_position < 0:
+            new_position += 26
+        cipher += alphabet[new_position]
+    print(f"The decoded text is {cipher}")
+
+if direction == "encode":
+    encrypt(message, shift)
+else:
+    decrypt(message, shift)
+
+
